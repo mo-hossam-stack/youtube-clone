2f167906ef6d7c07599b49bdb12141e756249ea1
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-03 17:28:52 +0300

    feat: integrate ImageKit for video and thumbnail upload functionality in video views

---
+++ backend/videos/helpers.py
@@ -0,0 +1,55 @@
+import os
+from imagekitio import ImageKit
+
+
+def get_imagekit_client():
+    return ImageKit()
+
+
+def upload_video(file_data: bytes, file_name: str, folder: str = "videos") -> dict:
+    public_key = os.environ.get("IMAGEKIT_PUBLIC_KEY")
+
+    client = get_imagekit_client()
+
+    response = client.files.upload(
+        file=file_data,
+        file_name=file_name,
+        folder=folder,
+        public_key=public_key
+    )
+
+    return {
+        "file_id": response.file_id,
+        "url": response.url
+    }
+
+
+def upload_thumbnail(file_data: bytes, file_name: str, folder: str = "thumbnails") -> dict:
+    import base64
+    public_key = os.environ.get("IMAGEKIT_PUBLIC_KEY")
+
+    if file_data.startswith("data:"):
+        base64_data = file_data.split(",", 1)[1]
+        image_bytes = base64.b64decode(base64_data)
+    else:
+        image_bytes = base64.b64decode(file_data)
+
+    client = get_imagekit_client()
+
+    response = client.files.upload(
+        file=image_bytes,
+        file_name=file_name,
+        folder=folder,
+        public_key=public_key
+    )
+
+    return {
+        "file_id": response.file_id,
+        "url": response.url
+    }
+
+
+def delete_video(file_id: str) -> bool:
+    client = get_imagekit_client()
+    client.files.delete(file_id=file_id)
+    return True
